34883f616c9698818dd8c50366a7642dc34d9682
madhumerr <madhumercur@gmail.com>
2025-01-25 10:30:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todo';
+  tasks:string[]=[]
+  task_text:string=''
+
+  addText(event:any){
+    this.task_text=event.target.value
+
+  }
+  addTask(){
+    if(this.task_text==''){
+      alert('Please enter task')
+    }
+    else{
+      this.tasks.push(this.task_text)
+    let v:any=document.getElementById('inp')
+    v.value=''
+    this.task_text=''
+    }     
+  }
+  deleteTask(ind:any){
+    this.tasks.splice(ind,1 )
+  }
 }
